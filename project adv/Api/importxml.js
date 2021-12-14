fdede51f4a6bf1ba4472a7325296a601ea0ea314
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2021-12-14 18:14:49 +0200

    Update importxml.js

--- project adv/Api/importxml.js
+++ project adv/Api/importxml.js
@@ -1,6 +1,6 @@
 
-function SetJson(jsonv){
-    const fs = require('fs');
+
+const fs = require('fs');
 const xml2js = require('xml2js');
 const util = require('util');
 const parser = new xml2js.Parser();
@@ -8,9 +8,12 @@ let x;
  fs.readFile('file.xml', (err, data) => {
 	parser.parseString(data, (err, result) => {
 		x=util.inspect(result, false,null,true);
+    SetJson(x)
     return result;
 	});
 });
+
+function SetJson(jsonv){
   y=x;
   console.log(y)
 }
