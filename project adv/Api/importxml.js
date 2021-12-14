e31ececa096a06fde33ab862b8c62b8ed127fff6
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2021-12-14 18:04:17 +0200

    Add files via upload

---
+++ project adv/Api/importxml.js
@@ -0,0 +1,16 @@
+
+function SetJson(jsonv){
+    const fs = require('fs');
+const xml2js = require('xml2js');
+const util = require('util');
+const parser = new xml2js.Parser();
+let x;
+ fs.readFile('file.xml', (err, data) => {
+	parser.parseString(data, (err, result) => {
+		x=util.inspect(result, false,null,true);
+    return result;
+	});
+});
+  y=x;
+  console.log(y)
+}
